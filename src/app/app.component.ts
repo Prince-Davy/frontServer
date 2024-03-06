1a76be21b9a0192a6d5904c825e2d6af04e8b6f8
Prince-Davy <princedavy.nkakou@gmail.com>
2024-03-06 16:38:22 +0100

    Inhjection Du Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ServerService } from './service/server.service';
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { DataState } from './enum/data-state.enum';
+
+
 
 @Component({
   selector: 'app-root',
@@ -9,6 +16,30 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'serverapp';
+  appState$!: Observable<AppState<CustomResponse>>;
+
+  constructor(private serverService : ServerService){
+  }
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.server$
+    .pipe(
+      map(response => {
+        return {dataState: DataState.LOADED_STATE, appData: response}
+      }),
+      startWith(
+        {dataState: DataState.LOADIND_STATE}
+        ),
+        catchError((error: string) => {
+          return of({
+            dataState: DataState.ERROR_STATE,
+            error
+          })
+        })
+    );
+  }
+
 }
+
